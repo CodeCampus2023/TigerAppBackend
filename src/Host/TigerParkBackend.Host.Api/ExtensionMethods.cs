75ad5da8cfd3e7d5c7298e209d69693f8e54c463
Ilya Alexandrov <pukisona12@mail.ru>
2023-05-13 22:08:34 +0300

    Added DbContext and configuration

---
+++ src/Host/TigerParkBackend.Host.Api/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TigerParkBackend.Infrastructure.DataAccess;
+using TigerParkBackend.Infrastructure.DataAccess.Interfaces;
+
+namespace TigerParkBackend.Host.Api;
+
+/// <summary>
+/// Методы расширения для класса <see cref="Program"/>
+/// </summary>
+public static class ExtensionMethods
+{
+    /// <summary>
+    /// Добавить DbContext с конфигурациями в DI
+    /// </summary>
+    /// <param name="serviceCollection">Сервисы программы</param>
+    public static void AddDbContextWithConfigurations(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IDbContextOptionsConfigurator<AppDbContext>, AppDbContextConfiguration>();
+        serviceCollection.AddDbContext<AppDbContext>((Action<IServiceProvider, DbContextOptionsBuilder>)
+            ((sp, dbOptions) => sp.GetRequiredService<IDbContextOptionsConfigurator<AppDbContext>>()
+                .Configure((DbContextOptionsBuilder<AppDbContext>)dbOptions)));
+        serviceCollection.AddScoped((Func<IServiceProvider, DbContext>) (sp => sp.GetRequiredService<AppDbContext>()));
+    }
+}
---
+++ src/Infrastructure/TigerParkBackend.Infrastructure.DataAccess/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace TigerParkBackend.Infrastructure.DataAccess;
+
+/// <summary>
+/// Основной контекст данных
+/// </summary>
+public class AppDbContext : DbContext
+{
+    /// <summary>
+    /// Конструктор <see cref="AppDbContext"/>
+    /// </summary>
+    /// <param name="options">Настройки контекста</param>
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly(), t => t.GetInterfaces().Any(i =>
+            i.IsGenericType &&
+            i.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>)));
+    }
+}
